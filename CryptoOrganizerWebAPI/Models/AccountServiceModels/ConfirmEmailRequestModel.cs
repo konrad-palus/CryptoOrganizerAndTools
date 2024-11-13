dc81c842e4ba07a606a501fbaecc65b48b9bc094
konrad-palus <117956112+konrad-palus@users.noreply.github.com>
2024-11-13 22:52:43 +0100

    forgot model

---
+++ CryptoOrganizerWebAPI/Models/AccountServiceModels/ConfirmEmailRequestModel.cs
@@ -0,0 +1,10 @@
+﻿using System.ComponentModel.DataAnnotations;
+namespace CryptoOrganizerWebAPI.Models.AccountServiceModels;
+
+public class ConfirmEmailRequestModel
+{
+    [Required, EmailAddress]
+    public string Email { get; set; }
+    [Required]
+    public string Token { get; set; }
+}
