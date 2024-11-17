46c622be119836b7ef418b151a281bba454e5285
konrad-palus <117956112+konrad-palus@users.noreply.github.com>
2024-11-17 19:51:46 +0100

    Account Interface

---
+++ CryptoOrganizerWebAPI/Interfaces/IAccountService.cs
@@ -0,0 +1,12 @@
+﻿using CryptoOrganizerWebAPI.Models.AccountServiceModels;
+using CryptoOrganizerWebAPI.Models.Responses;
+namespace CryptoOrganizerWebAPI.Interfaces;
+
+public interface IAccountService
+{
+    Task<ServiceResult> RegisterUserAsync(RegistrationRequestModel registrationRequest);
+    Task<ServiceResult> LoginUserAsync(LoginRequestModel loginRequest);
+    Task<ServiceResult> ConfirmUserEmailAsync(ConfirmEmailRequestModel confirmEmailRequest);
+    Task<ServiceResult> ForgotPasswordAsync(ForgotPasswordRequestModel forgotPasswordRequest);
+    Task<ServiceResult> ResetPasswordAsync(ResetPasswordRequestModel resetPasswordRequest);
+}
