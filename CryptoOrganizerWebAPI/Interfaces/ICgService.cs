362749c2ca843d1b7e0649e4b6e7e5c85fcedfd0
konrad-palus <117956112+konrad-palus@users.noreply.github.com>
2024-11-26 20:09:08 +0100

    CoinGecko Interface

---
+++ CryptoOrganizerWebAPI/Interfaces/ICgService.cs
@@ -0,0 +1,9 @@
+﻿using Domain.Entities;
+
+namespace CryptoOrganizerWebAPI.Interfaces;
+
+public interface ICgService
+{
+    Task FetchLiquidityPoolsFromCoinGecko();
+    Task FetchTokensFromCoinGecko(int limit = 1, string slug = "bitcoin");
+}
