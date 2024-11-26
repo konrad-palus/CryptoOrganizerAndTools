091e3ecd4c3fa422256e2ac1fc050f3fc9652602
konrad-palus <117956112+konrad-palus@users.noreply.github.com>
2024-11-26 20:11:27 +0100

    program cs

--- CryptoOrganizerWebAPI/Program.cs
+++ CryptoOrganizerWebAPI/Program.cs
@@ -1,35 +1,65 @@
+using Application.Interfaces;
+using CryptoOrganizerWebAPI.Interfaces;
+using CryptoOrganizerWebAPI.Services;
+using Domain.Entities.User;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Mvc.Infrastructure;
+using Microsoft.EntityFrameworkCore;
+using Persistence;
+using Serilog;
 
-namespace CryptoOrganizerWebAPI;
-
-public class Program
+namespace CryptoOrganizerWebAPI
 {
-    public static void Main(string[] args)
+    public static class Program
     {
-        var builder = WebApplication.CreateBuilder(args);
-
-        // Add services to the container.
-
-        builder.Services.AddControllers();
-        // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
-        builder.Services.AddEndpointsApiExplorer();
-        builder.Services.AddSwaggerGen();
-
-        var app = builder.Build();
-
-        // Configure the HTTP request pipeline.
-        if (app.Environment.IsDevelopment())
+        public static void Main(string[] args)
         {
-            app.UseSwagger();
-            app.UseSwaggerUI();
+            var builder = WebApplication.CreateBuilder(args);
+
+            Log.Logger = new LoggerConfiguration()
+                .ReadFrom.Configuration(builder.Configuration)
+                .Enrich.FromLogContext()
+                .WriteTo.Console()
+                .WriteTo.File("./Logs/logfile.log", rollingInterval: RollingInterval.Day)
+                .CreateLogger();
+
+            builder.Host.UseSerilog();
+
+            builder.Services.AddDbContext<CryptoOrganizerContext>(options => options.UseInMemoryDatabase("CryptoOrganizerDatabase"));
+
+            builder.Services.AddIdentity<CryptoOrganizerUser, IdentityRole>()
+                            .AddEntityFrameworkStores<CryptoOrganizerContext>()
+                            .AddDefaultTokenProviders();
+
+            builder.Services.AddMemoryCache();
+
+            builder.Services.AddScoped<ICryptoOrganizerContext>(provider => provider.GetRequiredService<CryptoOrganizerContext>());
+            builder.Services.AddScoped<IAccountService, AccountService>();
+            builder.Services.AddScoped<IEmailService, EmailService>();
+            builder.Services.AddScoped<IUserService, UserService>();
+            builder.Services.AddScoped<ICgService, CgService>();
+            builder.Services.AddSingleton<IActionContextAccessor, ActionContextAccessor>();
+            builder.Services.AddSingleton<ICacheService, CacheService>();
+
+
+            builder.Services.AddControllers();
+            builder.Services.AddEndpointsApiExplorer();
+            builder.Services.AddSwaggerGen();
+
+            var app = builder.Build();
+
+            if (app.Environment.IsDevelopment())
+            {
+                app.UseSwagger();
+                app.UseSwaggerUI();
+            }
+
+            app.UseHttpsRedirection();
+            app.UseAuthentication();
+            app.UseAuthorization();
+            app.MapControllers();
+
+            app.Run();
         }
-
-        app.UseHttpsRedirection();
-
-        app.UseAuthorization();
-
-
-        app.MapControllers();
-
-        app.Run();
     }
 }
