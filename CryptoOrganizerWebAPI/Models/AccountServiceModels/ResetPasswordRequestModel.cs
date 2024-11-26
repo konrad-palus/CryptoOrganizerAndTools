5cee7067245bda2af6bb96958da4918229395681
konrad-palus <117956112+konrad-palus@users.noreply.github.com>
2024-11-26 20:09:38 +0100

    ResetPasswordModel update

--- CryptoOrganizerWebAPI/Models/AccountServiceModels/ResetPasswordRequestModel.cs
+++ CryptoOrganizerWebAPI/Models/AccountServiceModels/ResetPasswordRequestModel.cs
@@ -3,10 +3,6 @@ namespace CryptoOrganizerWebAPI.Models.AccountServiceModels;
 
 public class ResetPasswordRequestModel
 {
-    [Required, EmailAddress]
-    public string Email { get; set; }
-    [Required]
-    public string Token { get; set; }
-    [Required, MinLength(6, ErrorMessage = "Password must be at least 6 characters long")]
     public string Password { get; set; }
+    public string ConfirmPassword { get; set; }
 }
