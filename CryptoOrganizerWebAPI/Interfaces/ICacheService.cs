fee69b28399475c844465dd65d5a05f8f328df36
konrad-palus <117956112+konrad-palus@users.noreply.github.com>
2024-11-26 20:08:59 +0100

    Cache interface

---
+++ CryptoOrganizerWebAPI/Interfaces/ICacheService.cs
@@ -0,0 +1,10 @@
+﻿namespace CryptoOrganizerWebAPI.Interfaces
+{
+    public interface ICacheService
+    {
+        void Set<T>(string key, T data, TimeSpan? expiry = null);
+        T? Get<T>(string key);
+        void Remove(string key);
+        bool Exists(string key);
+    }
+}
