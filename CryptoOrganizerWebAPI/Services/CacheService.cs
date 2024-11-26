867e311dded403ad6f24f49ef20d1e989e266363
konrad-palus <117956112+konrad-palus@users.noreply.github.com>
2024-11-26 20:10:15 +0100

    Cache Service implementation

---
+++ CryptoOrganizerWebAPI/Services/CacheService.cs
@@ -0,0 +1,21 @@
+﻿using CryptoOrganizerWebAPI.Interfaces;
+using Microsoft.Extensions.Caching.Memory;
+
+namespace CryptoOrganizerWebAPI.Services
+{
+    public class CacheService(IMemoryCache cache) : ICacheService
+    {
+        public void Set<T>(string key, T data, TimeSpan? expiry = null)
+        {
+            var options = new MemoryCacheEntryOptions().SetAbsoluteExpiration(expiry ?? TimeSpan.FromMinutes(5));
+
+            cache.Set(key, data, options);
+        }
+
+        public T Get<T>(string key) => cache.TryGetValue(key, out T data) ? data : default;
+
+        public void Remove(string key) => cache.Remove(key);
+
+        public bool Exists(string key) => cache.TryGetValue(key, out _);
+    }
+}
