fb605fd06df1920e26db55a508b5dfb8e7c3a20e
konrad-palus <117956112+konrad-palus@users.noreply.github.com>
2024-11-26 20:12:46 +0100

    Persistence

---
+++ Persistence/CryptoOrganizerContext.cs
@@ -0,0 +1,17 @@
+﻿using Application.Interfaces;
+using Domain.Entities;
+using Domain.Entities.User;
+using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore;
+
+namespace Persistence
+{
+    public class CryptoOrganizerContext(DbContextOptions<CryptoOrganizerContext> options) : IdentityDbContext<CryptoOrganizerUser>(options), ICryptoOrganizerContext
+    {
+        public DbSet<CryptoOrganizerUser> Users { get; set; }
+        public DbSet<Token> Tokens { get; set; }
+        //public DbSet<Exchange> Exchanges { get; set; }
+        //public DbSet<LiquidityPool> LiquidityPools { get; set; }
+        public DbSet<Notification> Notifications { get; set; }
+    }
+}
