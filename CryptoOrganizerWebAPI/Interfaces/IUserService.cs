52ae28797187e574865bd91a06419421e1035789
konrad-palus <117956112+konrad-palus@users.noreply.github.com>
2024-11-26 20:09:29 +0100

    User Service Interface

---
+++ CryptoOrganizerWebAPI/Interfaces/IUserService.cs
@@ -0,0 +1,13 @@
+﻿using CryptoOrganizerWebAPI.Models.DTO;
+
+namespace CryptoOrganizerWebAPI.Interfaces;
+
+public interface IUserService
+{
+    Task<bool> AddFavoriteTokenAsync(string userId, int tokenId);
+    Task<bool> RemoveFavoriteTokenAsync(string userId, int tokenId);
+    Task<List<FavoriteTokenDto>> GetFavoriteTokensAsync(string userId);
+    Task NotifyArbitrageOpportunitiesAsync();
+    Task<List<NotificationDTO>> GetNotificationsAsync(string userId);
+    Task<bool> MarkNotificationAsReadAsync(string userId, int notificationId);
+}
