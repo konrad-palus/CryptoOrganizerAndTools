bf25a38d78ea566cd203bb0c5a1b533fe00322b6
konrad-palus <117956112+konrad-palus@users.noreply.github.com>
2024-11-26 20:12:23 +0100

    Context

---
+++ Application/Interfaces/ICryptoOrganizerContext.cs
@@ -0,0 +1,13 @@
+﻿using Domain.Entities;
+using Domain.Entities.User;
+using Microsoft.EntityFrameworkCore;
+
+namespace Application.Interfaces;
+public interface ICryptoOrganizerContext : IDbContext
+{
+    DbSet<CryptoOrganizerUser> Users { get; set; }
+    DbSet<Token> Tokens { get; set; }
+    //DbSet<Exchange> Exchanges { get; set; }
+    //DbSet<LiquidityPool> LiquidityPools { get; set; }
+    DbSet<Notification> Notifications { get; set; }
+}
