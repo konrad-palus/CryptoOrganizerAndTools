8452b9e89fc2ab0f3cfc39694901bb1432b11cdb
konrad-palus <117956112+konrad-palus@users.noreply.github.com>
2025-01-08 07:01:42 +0100

    Last Update added to db

--- Domain/Entities/LiquidityPool.cs
+++ Domain/Entities/LiquidityPool.cs
@@ -5,8 +5,10 @@ public class LiquidityPool
     //public Token Token { get; set; }
     public string TokenTicker { get; set; }
     //public Exchange Exchange { get; set; }
+    public string TradeUrl { get; set; }
     public string ExchangeName { get; set; }
+    //public string ExchangeLogoUrl { get; set; }
     public decimal BuyPrice { get; set; }
     //public decimal SellPrice { get; set; }
-    //public DateTime LastUpdated { get; set; }
+    public string LastUpdated { get; set; }
 }
